008f30897f0e5579d37d75199f9e406b20d559d8
wdEffort <yoman3011@gmail.com>
2021-09-20 13:02:53 +0900

    Jackson2 library 테스트 컨트롤러 작성

--- src/main/java/kr/or/connect/springwebsocket/controller/ChatController.java
+++ src/main/java/kr/or/connect/springwebsocket/controller/ChatController.java
@@ -1,8 +1,12 @@
 package kr.or.connect.springwebsocket.controller;
 
+import kr.or.connect.springwebsocket.vo.StompMessage;
 import kr.or.connect.springwebsocket.vo.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -18,5 +22,11 @@ public class ChatController {
         }
 
         return "chat/chat";
+    }
+
+    @ResponseBody
+    @PostMapping("/chat/api")
+    public StompMessage chatPage(@RequestBody StompMessage message) {
+        return message;
     }
 }
